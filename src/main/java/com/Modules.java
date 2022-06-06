ec08a30f9cb9b2b8a936dd181fc2e7f941d74ecd
mrbear22 <theendgamermc@gmai.com>
2022-06-06 19:11:55 +0300

    Add some classes

---
+++ src/main/java/com/Modules.java
@@ -0,0 +1,12 @@
+package com;
+
+public class Modules {
+
+	public void enableBungeeModules() {
+		
+	}
+	
+	public void enableSpigotModules() {
+		
+	}
+}
---
+++ src/main/java/objects/BreadMaker.java
@@ -0,0 +1,15 @@
+package objects;
+
+import java.util.Arrays;
+
+public class BreadMaker {
+
+	private String[] options = {"username", "level"};
+	
+	public void getData(String option) {
+		
+		Arrays.asList(options).indexOf(option);
+		
+	}
+	
+}
