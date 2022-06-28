c0a05f546f6ebc1152f6db62fe2611aabfb71451
valf2k3 <valf2k3@gmail.com>
2022-06-28 15:55:01 +0300

    Зарегал тп и ендерчест, но все равно проверяйте сами сообщения сами

--- src/main/java/com/BrainSpigot.java
+++ src/main/java/com/BrainSpigot.java
@@ -2,6 +2,8 @@ package com;
 
 import java.util.HashMap;
 
+import commands.EnderchestCommand;
+import commands.TeleportCommands;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import commands.Info;
@@ -16,9 +18,11 @@ public class BrainSpigot extends JavaPlugin {
 		
 		getServer().getMessenger().registerOutgoingPluginChannel(this, "ArchiQuest");
 		getServer().getMessenger().registerIncomingPluginChannel(this, "ArchiQuest", new SystemMessageReceiver(this));
-		
+
 		getCommand("bread").setExecutor(new Info(this, "archiquest.player"));
-		
+		getCommand("tp").setExecutor(new TeleportCommands(this));
+		getCommand("enderchest").setExecutor(new EnderchestCommand(this));
+
 		getLogger().info("archiquestcore is ready to be helpful for all beadmakers!");
 	}
 
